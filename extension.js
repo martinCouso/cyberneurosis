831bb4165ca8b2387f90076b2cf5caaf6c0ca3c8
Martín Couso <martin.couso@gmail.com>
2025-03-16 15:19:08 -0400

    chore(Theme): initial commit

--- extension.js
+++ extension.js
@@ -11,30 +11,30 @@ const removeStyles = ()=> {
   const htmlFile = path.join(base, electronBase, "workbench", workbenchFilename);
   try {
     let html = fs.readFileSync(htmlFile, "utf8");
-    if (html.includes("neonmemories.js")) {
-      html = html.replace(/\s*<!-- CUSTOM THEME -->\s*<script src="neonmemories\.js"><\/script>\s*\n?/, '');
+    if (html.includes("cyberneurosis.js")) {
+      html = html.replace(/\s*<!-- CUSTOM THEME -->\s*<script src="cyberneurosis\.js"><\/script>\s*\n?/, '');
       fs.writeFileSync(htmlFile, html, "utf8");
 
-      const targetPath = path.join(base, electronBase, "workbench", "neonmemories.js");
+      const targetPath = path.join(base, electronBase, "workbench", "cyberneurosis.js");
       if (fs.existsSync(targetPath)) {
         fs.unlinkSync(targetPath);
       }
 
-      vscode.window.showInformationMessage("Neon Memories: Neon Mode terminated. VS Code must reload to apply changes.", { title: "Reload" })
+      vscode.window.showInformationMessage("Cyberneurosis: Neon Mod removed. VS Code must reload to apply changes.", { title: "Reload" })
         .then(() => vscode.commands.executeCommand("workbench.action.reloadWindow"));
     } else {
-      vscode.window.showInformationMessage("Neon Memories: Neon Mode was not initiated.");
+      vscode.window.showInformationMessage("Cyberneurosis: Neon Mod was not implanted.");
     }
   } catch (e) {
-    vscode.window.showErrorMessage("Neon Memories: Error terminating Neon Mode: either the mode was too strong or it's related to the following error: " + e.message);
+    vscode.window.showErrorMessage("Cyberneurosis: Error terminating Neon Mod: either the mode was too strong or it's related to the following error: " + e.message);
   }
 }
 
 
 function activate(context) {
-  console.info('Neon Memories extension is now active!'); // Debugging
+  console.info('Cyberneurosis extension is now active!'); // Debugging
 
-  const config = vscode.workspace.getConfiguration("neonmemories");
+  const config = vscode.workspace.getConfiguration("cyberneurosis");
   const disableGlow = !!config.disableGlow;
 
   const glowCssPath = path.join(__dirname, 'css', 'glow.css');
@@ -48,8 +48,7 @@ function activate(context) {
   const workbenchFilename = vscode.version.startsWith("1.94") ? "workbench.esm.html" : "workbench.html";
   const htmlFile = path.join(base, electronBase, "workbench", workbenchFilename);
 
-  let disposable = vscode.commands.registerCommand('neonmemories.initiateNeonMode', function () {
-    console.log('Neon Memories: Initiate Neon Mode command activated!');
+  let disposable = vscode.commands.registerCommand('cyberneurosis.implantNeonMod', function () {
 
     const templatePath = path.join(__dirname, 'js', 'theme_template.js');
     let jsTemplate = fs.readFileSync(templatePath, 'utf8');
@@ -58,67 +57,67 @@ function activate(context) {
     jsTemplate = jsTemplate.replace(/\[CSS_WITH_GLOW\]/g, cssWithGlow.replace(/(\r\n|\n|\r)/gm, ''));
     jsTemplate = jsTemplate.replace(/\[CSS_WITHOUT_GLOW\]/g, cssWithoutGlow.replace(/(\r\n|\n|\r)/gm, ''));
 
-    const outputPath = path.join(__dirname, 'js', 'neonmemories.js');
-    const targetPath = path.join(base, electronBase, "workbench", "neonmemories.js");
+    const outputPath = path.join(__dirname, 'js', 'cyberneurosis.js');
+    const targetPath = path.join(base, electronBase, "workbench", "cyberneurosis.js");
     fs.writeFileSync(outputPath, jsTemplate, 'utf8');
     fs.copyFileSync(outputPath, targetPath);  
 
     try {
       let html = fs.readFileSync(htmlFile, "utf8");
 
-      if (!html.includes("neonmemories.js")) {
-        html = html.replace(/\<\/html\>/g, `  <!-- CUSTOM THEME -->\n  <script src="neonmemories.js"></script>\n</html>`);
+      if (!html.includes("cyberneurosis.js")) {
+        html = html.replace(/\<\/html\>/g, `  <!-- CUSTOM THEME -->\n  <script src="cyberneurosis.js"></script>\n</html>`);
         fs.writeFileSync(htmlFile, html, "utf8");
 
-        vscode.window.showInformationMessage("Neon Memories: Initiating Neon Mode. The editor must reload to apply changes.", { title: "Reload" })
+        vscode.window.showInformationMessage("Cyberneurosis: Implanting Neon Mod. The editor must reload to apply changes.", { title: "Reload" })
           .then(() => vscode.commands.executeCommand("workbench.action.reloadWindow"));
       } else {
-        vscode.window.showInformationMessage("Neon Memories: Mode was already activated, check your memory module. Reload to refresh settings.", { title: "Reload" })
+        vscode.window.showInformationMessage("Cyberneurosis: Mod was already implanted, check your memory module. Reload to refresh settings.", { title: "Reload" })
           .then(() => vscode.commands.executeCommand("workbench.action.reloadWindow"));
       }
     } catch (e) {
-      vscode.window.showErrorMessage("Neon Memories: Error while initianing Neon Mode: " + e.message);
+      vscode.window.showErrorMessage("Cyberneurosis: Error while implanting Neon Mod: " + e.message);
     }
   }); 
 
   context.subscriptions.push(disposable);
 
 
-  let disposableTerminate = vscode.commands.registerCommand('neonmemories.terminateNeonMode', function () {
-    console.info('Neon Memories: Terminate Neon Mode command executed.');
+  let disposableRemove = vscode.commands.registerCommand('cyberneurosis.removeNeonMod', function () {
+    console.info('Cyberneurosis: Remove Neon Mod command executed.');
     try {
       let html = fs.readFileSync(htmlFile, "utf8");
-      if (html.includes("neonmemories.js")) {
-        html = html.replace(/\s*<!-- CUSTOM THEME -->\s*<script src="neonmemories\.js"><\/script>\s*\n?/, '');
+      if (html.includes("cyberneurosis.js")) {
+        html = html.replace(/\s*<!-- CUSTOM THEME -->\s*<script src="cyberneurosis\.js"><\/script>\s*\n?/, '');
         fs.writeFileSync(htmlFile, html, "utf8");
   
-        const targetPath = path.join(base, electronBase, "workbench", "neonmemories.js");
+        const targetPath = path.join(base, electronBase, "workbench", "cyberneurosis.js");
         if (fs.existsSync(targetPath)) {
           fs.unlinkSync(targetPath);
         }
   
-        vscode.window.showInformationMessage("Neon Memories: Neon Mode terminated. The editor must reload to apply changes.", { title: "Reload" })
+        vscode.window.showInformationMessage("Cyberneurosis: Neon Mod removed. The editor must reload to apply changes.", { title: "Reload" })
           .then(() => vscode.commands.executeCommand("workbench.action.reloadWindow"));
       } else {
-        vscode.window.showInformationMessage("Neon Memories: Neon Mode was not initiated.");
+        vscode.window.showInformationMessage("Cyberneurosis: Neon Mod was not implanted.");
       }
     } catch (e) {
-      vscode.window.showErrorMessage("Neon Memories: Error terminating Neon Mode: either the mode was too strong or it's related to the following error: " + e.message);
+      vscode.window.showErrorMessage("Cyberneurosis: Error terminating Neon Mod: either the mode was too strong or it's related to the following error: " + e.message);
     } 
   });
-  context.subscriptions.push(disposableTerminate);
+  context.subscriptions.push(disposableRemove);
 } 
 
 exports.activate = activate;
 
 function deactivate() {
-  console.info('Neon Memories: Extension is being deactivated');
+  console.info('Cyberneurosis: Extension is being deactivated');
   //removeStyles();
 }
 
 
 function uninstall() {
-  console.info('Neon Memories: Extension is being uninstalled.');
+  console.info('Cyberneurosis: Extension is being uninstalled.');
   //removeStyles();
 }
 
